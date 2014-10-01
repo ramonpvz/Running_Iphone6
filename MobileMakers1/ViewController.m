b32aad8334a0c326365f003cf3e28d21b537069c
Ramon <ramon.pinedav@gmail.com>
2014-10-01 13:00:45 -0500

    Second commit

--- MobileMakers1/ViewController.m
+++ MobileMakers1/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#define HOME_URL @"http://www.mobilemakers.co"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate , UITextFieldDelegate, UIAlertViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
 
 @end
 
@@ -16,12 +19,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//     Do any additional setup after loading the view, typically from a nib.
+//    NSString *urlString = @"http://www.mobilemakers.co";
+//    NSURL *url = [NSURL URLWithString:urlString];
+//    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+//    [self.webView loadRequest:urlRequest];
+//    [self loadURLString:@"http://www.mobilemakers.co"];
+    [self loadHomePage];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
 }
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    UIAlertView *alertView = [[UIAlertView alloc] init];
+    alertView.title = @"Bad Error... No Biscuit";
+    alertView.message = error.localizedDescription;
+    [alertView addButtonWithTitle:@"That sucks!!!!!!!!!!!!"];
+    [alertView addButtonWithTitle:@"Go Home"];
+    [alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        NSString *urlString = @"http://www.google.com";
+        NSURL *url = [NSURL URLWithString:urlString];
+        NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:urlRequest];
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString *urlString = textField.text;
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:urlRequest];
+    return YES;
+}
+
+- (void) loadURLString: (NSString *) urlString {
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:urlRequest];
+}
+
+- (void) loadHomePage {
+    [self loadURLString:HOME_URL];
+}
+
+
 
 @end
